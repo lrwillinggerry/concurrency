dbd3dcce53af242281871650ce355c31b348f3a5
lrwillinggerry <lrwillinggerry@outlook.com>
2018-12-14 15:53:40 +0800

    add annoations

---
+++ src/main/java/com/mmall/concurrency/annoations/Recommend.java
@@ -0,0 +1,23 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author rongjianrong
+ * @date 2018-12-14
+ */
+
+/**
+ * 用于标记【推荐】的类或者写法
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Recommend {
+
+    String value() default "";
+
+}
